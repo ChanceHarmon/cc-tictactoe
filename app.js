c96067b2958843e983771d2d8f93ef777d89f0ae
Chance Harmon <henrikillsit@gmail.com>
2019-08-22 23:03:54 -0700

    its a real function

--- app.js
+++ app.js
@@ -9,23 +9,26 @@ let ticTacToe = [
   ['x', 'o', 'o'],
   ['x', 'o', 'x']
 ];
+function tallyWins() {
+  for (let i = 0; i < ticTacToe.length; i++) {
+    for (let j = 0; j < ticTacToe[i].length; j++);
 
-for (let i = 0; i < ticTacToe.length; i++) {
-  for (let j = 0; j < ticTacToe[i].length; j++);
 
+    if (ticTacToe[0][0] && ticTacToe[0][1] && ticTacToe[0][2] || ticTacToe[1][0] && ticTacToe[1][1] && ticTacToe[1][2] || ticTacToe[2][0] && ticTacToe[2][1] && ticTacToe[2][2] || ticTacToe[0][0] && ticTacToe[1][0] && ticTacToe[2][0] || ticTacToe[0][1] && ticTacToe[1][1] && ticTacToe[2][1] || ticTacToe[0][2] && ticTacToe[1][2] && ticTacToe[2][2] || ticTacToe[0][0] && ticTacToe[1][1] && ticTacToe[2][2] || ticTacToe[0][2] && ticTacToe[1][1] && ticTacToe[2][0] === 'x') {
+      counterX++
+    };
 
-  if (ticTacToe[0][0] && ticTacToe[0][1] && ticTacToe[0][2] || ticTacToe[1][0] && ticTacToe[1][1] && ticTacToe[1][2] || ticTacToe[2][0] && ticTacToe[2][1] && ticTacToe[2][2] || ticTacToe[0][0] && ticTacToe[1][0] && ticTacToe[2][0] || ticTacToe[0][1] && ticTacToe[1][1] && ticTacToe[2][1] || ticTacToe[0][2] && ticTacToe[1][2] && ticTacToe[2][2] || ticTacToe[0][0] && ticTacToe[1][1] && ticTacToe[2][2] || ticTacToe[0][2] && ticTacToe[1][1] && ticTacToe[2][0] === 'x') {
-    counterX++
-  };
+    if (ticTacToe[0][0] && ticTacToe[0][1] && ticTacToe[0][2] || ticTacToe[1][0] && ticTacToe[1][1] && ticTacToe[1][2] || ticTacToe[2][0] && ticTacToe[2][1] && ticTacToe[2][2] || ticTacToe[0][0] && ticTacToe[1][0] && ticTacToe[2][0] || ticTacToe[0][1] && ticTacToe[1][1] && ticTacToe[2][1] || ticTacToe[0][2] && ticTacToe[1][2] && ticTacToe[2][2] || ticTacToe[0][0] && ticTacToe[1][1] && ticTacToe[2][2] || ticTacToe[0][2] && ticTacToe[1][1] && ticTacToe[2][0] === 'y') {
+      counterY++
+    }
 
-  if (ticTacToe[0][0] && ticTacToe[0][1] && ticTacToe[0][2] || ticTacToe[1][0] && ticTacToe[1][1] && ticTacToe[1][2] || ticTacToe[2][0] && ticTacToe[2][1] && ticTacToe[2][2] || ticTacToe[0][0] && ticTacToe[1][0] && ticTacToe[2][0] || ticTacToe[0][1] && ticTacToe[1][1] && ticTacToe[2][1] || ticTacToe[0][2] && ticTacToe[1][2] && ticTacToe[2][2] || ticTacToe[0][0] && ticTacToe[1][1] && ticTacToe[2][2] || ticTacToe[0][2] && ticTacToe[1][1] && ticTacToe[2][0] === 'y') {
-    counterY++
+    else {
+      counterDraw++
+    }
   }
-
-  else {
-    counterDraw++
-  }
+  console.log(counterX);
+  console.log(counterO);
+  console.log(counterDraw)
 }
-console.log(counterX);
-console.log(counterO);
-console.log(counterDraw)
+
+tallyWins();
